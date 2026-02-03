f02ef676744553ba2fb0b70ad12133f87fcbcad1
vineshshetty27 <vineshshetty2004@gmail.com>
2026-02-03 12:54:48 +0530

    Create footer.jsx

---
+++ LAB2/footer.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'; 
+const Footer = ({ copyright }) => { 
+return ( 
+<footer> 
+<p>{copyright}</p> 
+</footer> 
+); 
+}; 
+Footer.propTypes = { 
+copyright: PropTypes.string.isRequired, }; 
+export default Footer; 
